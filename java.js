742bc79cbd42f08bab0b7b846c625f1060869e76
Pravlad <vlado.sieklik@gmail.com>
2017-11-30 11:30:49 +0100

    Js hodiny

--- java.js
+++ java.js
@@ -7,6 +7,20 @@
       dat.getFullYear();
       return vrat;
   } 	   
+function tiktak(){
+var cas=new Date()
+var hodina=cas.getHours()
+var minuta=cas.getMinutes()
+var sekunda=cas.getSeconds()
 
+if (minuta<10)
+minuta="0"+minuta
+
+if (sekunda<10)
+sekunda="0"+sekunda
+
+document.zobraz.hodiny.value=hodina+":"+minuta+":"+sekunda
+setTimeout("tiktak()",1000)
+}
 	   
 	   

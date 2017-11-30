0eac8715168bb29b2e962fcc208855bdc08264a7
Pravlad <vlado.sieklik@gmail.com>
2017-11-30 11:08:01 +0100

    Posledna aktualizacia js

--- java.js
+++ java.js
@@ -15,6 +15,14 @@ setTimeout("tiktak()",1000)
 }
 tiktak()
 	   
-	   
+  function datum() { 
+      var dat = new Date();
+      dat.setTime(Date.parse(document.lastModified));
+      var mesiac=1;
+      mesiac=mesiac+dat.getMonth();
+      var vrat = dat.getDate() + "." + mesiac + "." +
+      dat.getFullYear();
+      return vrat;
+  } 	   
 	   
 	   

222f1601b8ca4c9fc899ac55f10d6edc288114ff
Pravlad <vlado.sieklik@gmail.com>
2017-12-14 11:18:26 +0100

    DAtum

--- java.js
+++ java.js
@@ -37,5 +37,5 @@ setTimeout("tiktak()",1000)
     var curr_date = d.getDate();
     var curr_month = d.getMonth() + 1; //months are zero based
     var curr_year = d.getFullYear();
-    document.write(curr_date + "-" + curr_month + "-" + curr_year);
+    document.write("Dnešný Dátum: " + curr_date + "." + curr_month + "." + curr_year);
   }

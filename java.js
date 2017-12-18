ceb7aedeb523dbbea02ebb0be7dfbcda48ecb96b
Pravlad <vlado.sieklik@gmail.com>
2017-12-18 09:28:07 +0100

    JS link

--- java.js
+++ java.js
@@ -29,6 +29,7 @@ setTimeout("tiktak()",1000)
     quotes[1] = "Baseball has the great advantage over cricket of being sooner ended.";
     quotes[2] = "Every goal, every action, every thought, every feeling one experiences, whether it be consciously or unconsciously known, is an attempt to increase one’s level of peace of mind.";
     quotes[3] = "A good head and a good heart are always a formidable combination.";
+	quotes[4] = "Je tažké byť autistom.";
     var rand = Math.floor(Math.random()*quotes.length);
     document.write(quotes[rand]);
   }
